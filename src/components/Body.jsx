b57fc7248966e768ade252fe760fc979fa8d53c8
Gowthami <gowthamisukhavasi@yahoo.in>
2024-07-01 17:36:33 -0400

    episode-5

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,28 @@
 import resList from "../utils/data.json";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
-  console.log(resList);
+  const [listOfRestaurants, setlistOfRestaurants] = useState(resList);
+
   return (
     <div className="body-container">
       <input className="search-bar" type="search" placeholder="Search here" />
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setlistOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="restaurant-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
